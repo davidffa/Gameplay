defc4d0aa7df4e3011321407723f862ea17b0490
David Amorim <davidfilipeamorim@gmail.com>
2021-07-30 16:39:41 +0100

    Add Background linear gradient

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
+import { Background } from './src/components/Background';
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
@@ -20,13 +21,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SignIn />
-    </>
+    </Background>
   );
 }
