106a7ee6ea0de234956c21bd8b6b3ea8eafcb801
David Amorim <davidfilipeamorim@gmail.com>
2021-07-30 19:13:20 +0100

    Add Profile component

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Avatar } from '../Avatar';
+
+import { styles } from './styles';
+
+export function Profile() {
+  return (
+    <View style={styles.container}>
+      <Avatar urlImage="https://github.com/davidffa.png"/>
+
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>
+            Olá,
+          </Text>
+
+          <Text style={styles.username}>
+
+          </Text>
+        </View>
+        
+        <Text style={styles.message}>
+          Hoje é dia de vitória
+        </Text>
+      </View>
+    </View>
+  );
+}
