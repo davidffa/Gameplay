255b8c843f7c90714372db61067c6ba0fda5b96b
David Amorim <davidfilipeamorim@gmail.com>
2021-07-30 19:53:09 +0100

    Add content on Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,44 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+import { Appointment } from '../../components/Appointment';
 
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
+import { ListDivider } from '../../components/ListDivider';
+import { ListHeader } from '../../components/ListHeader';
 import { Profile } from '../../components/Profile';
 
 import { styles } from './styles';
 
 export function Home() {
   const [category, setCategory] = useState('');
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    }
+  ]
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -21,10 +51,26 @@ export function Home() {
         <ButtonAdd />
       </View>
 
-      <View>
-        <CategorySelect 
-          categorySelected={category}
-          setCategory={handleCategorySelect}
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+
+      <View style={styles.content}>
+        <ListHeader
+          title="Partidas agendadas"
+          subtitle="Total 6"
+        />
+
+        <FlatList
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Appointment data={item} />
+          )}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
         />
       </View>
     </View>
