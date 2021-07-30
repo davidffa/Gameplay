4aa97b0d0056eee60bee5d808ce9cf6d36f72edf
David Amorim <davidfilipeamorim@gmail.com>
2021-07-30 16:15:38 +0100

    Add SignIn screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SignIn />
   );
 }
