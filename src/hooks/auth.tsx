4b72087dd80980f5ffa35b71fde8c388697e0f23
David Amorim <davidfilipeamorim@gmail.com>
2021-07-31 19:50:09 +0100

    Add logout function

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -26,6 +26,7 @@ type AuthContextData = {
   user: User;
   loading: boolean;
   signIn: () => Promise<void>;
+  signOut: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -78,6 +79,11 @@ function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function signOut() {
+    setUser({} as User);
+    await AsyncStorage.removeItem(COLLECTION_USERS);
+  }
+
   async function loadUserStorageData() {
     const storage = await AsyncStorage.getItem(COLLECTION_USERS);
 
@@ -97,7 +103,8 @@ function AuthProvider({ children }: AuthProviderProps) {
     <AuthContext.Provider value={{
       user,
       loading,
-      signIn
+      signIn,
+      signOut
     }}>
       { children }
     </AuthContext.Provider>
