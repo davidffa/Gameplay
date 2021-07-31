ddfcc93108d0fdc4ce6d014b965de4229f93bd41
David Amorim <davidfilipeamorim@gmail.com>
2021-07-31 14:17:33 +0100

    Add AppointmentDetails screen

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '../screens/Home';
 import { SignIn } from '../screens/SignIn';
+import { AppointmentDetails } from '../screens/AppointmentDetails';
 
 import { theme } from '../global/styles/theme';
 
@@ -25,6 +26,10 @@ export function AuthRoutes() {
       <Screen 
         name="Home"
         component={Home}
+      />
+      <Screen 
+        name="AppointmentDetails"
+        component={AppointmentDetails}
       />
     </Navigator>
   );
